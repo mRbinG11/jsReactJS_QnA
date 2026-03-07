88a0753d1c2e06705150624677ab66c33d47221c
P Sujith Bhatt <sujithdon00797@gmail.com>
2026-03-07 19:16:22 +0530

    add flattenObject function to transform nested objects into a flat structure

---
+++ JS_Q1.js
@@ -0,0 +1,16 @@
+let input = {
+  a: 1,
+  b: { c: 2, d: { e: 3 } },
+};
+
+function flattenObject(obj, parentKey = "", result = {}) {
+  for (let key in obj) {
+    const newKey = parentKey ? parentKey + "." + key : key;
+    if (typeof obj[key] === "object" && obj[key] !== null)
+      flattenObject(obj[key], newKey, result);
+    else result[newKey] = obj[key];
+  }
+  return result;
+}
+
+console.log(flattenObject(input));
