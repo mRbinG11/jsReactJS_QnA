f599fe6a920e07b045df4cfeb5f6f8d5fd2060d8
P Sujith Bhatt <sujithdon00797@gmail.com>
2026-03-09 20:56:42 +0530

    login form comp added to App & updated App styling

--- RJS_Q/rjs-q17/src/App.js
+++ RJS_Q/rjs-q17/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+// App.js
+import React from "react";
+import LoginForm from "./components/LoginForm"; // Import the LoginForm component
+import "./App.css"; // Import global CSS styling
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1>Login</h1>
+      {/* Render the LoginForm inside the main app layout */}
+      <LoginForm />
     </div>
   );
-}
+};
 
 export default App;
