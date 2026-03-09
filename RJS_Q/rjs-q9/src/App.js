7d233c58fd1d6e51740e028390c4ba737a2e9b4a
P Sujith Bhatt <sujithdon00797@gmail.com>
2026-03-09 19:09:29 +0530

    card list comp added to App & updated App styling

--- RJS_Q/rjs-q9/src/App.js
+++ RJS_Q/rjs-q9/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+// App.js
+import React from "react";
+import CardList from "./components/CardList";
+import "./App.css";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h2>Efficiently Render 1000 Cards</h2>
+      <CardList />
     </div>
   );
-}
+};
 
 export default App;
