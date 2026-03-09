05edb48c7f51e262aca337dc06e7497caa4aa0d5
P Sujith Bhatt <sujithdon00797@gmail.com>
2026-03-09 20:36:11 +0530

    reverse str comp added to App & updated App styling

--- RJS_Q/rjs-q15/src/App.js
+++ RJS_Q/rjs-q15/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+// App.js
+import React from "react";
+import ReverseString from "./components/ReverseString"; // Import the ReverseString component
+import "./App.css"; // Import CSS styling
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1>Reverse a String</h1>
+      {/* Render the ReverseString component */}
+      <ReverseString />
     </div>
   );
-}
+};
 
 export default App;
